d540ecbab728b7a27026df67f2672332f33134b2
vbalalla <vbalalla@gmail.com>
2016-06-01 19:50:11 +0530

    added get my location function

--- index.php
+++ index.php
@@ -9,15 +9,19 @@
         <title>index</title>
 </head>
 <body background="weather.jpg">
-    <div class="wrapper" style="width: 425px;height: 300px;padding-top: 10px;margin: 10% auto;">
+    <div class="wrapper" style="width: 425px;height: 350px;padding-top: 10px;margin: 10% auto;">
     <div><h2 style="padding-left: 90px">weather Service</h2></div>
     <div class="container">
         <form action="control.php" method="post">
         <div class="form-group col-md-4">
             <label class="" >latitude: </label>
-            <input required class="form-control" type="text" name="lat"><br>
+            <input id="x" required class="form-control" type="text" name="lat"><br>
             <label class="" >longitude: </label>
-            <input required class="form-control" type="text" name="lon"><br>
+            <input id="y" required class="form-control" type="text" name="lon"><br>
+            <button onclick="getLocation()" class="btn btn-default">get my location</button>
+            <span id="error"></span>
+            </br>
+            <div style="padding-top: 20px"></div>
             <input class="btn btn-default" type="submit" name="submit" value="Results">
             <span style="color: red; padding-left: 20px"><?php if(isset($_GET['id'])){echo "Invalid input ! try again !";}?><span>
             <span style="color: red; padding-left: 20px"><?php if(isset($_GET['err'])){echo $_GET['err']." ! error !";}?><span>
@@ -25,6 +29,22 @@
         </form>
 
     </div>
+
+<script>
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        document.getElementById("error").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    document.getElementById("x").value = position.coords.latitude;
+    document.getElementById("y").value = position.coords.longitude;
+}
+</script>
 </body>
 
 </html>
